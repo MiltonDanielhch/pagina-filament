49d4b24c3b55c63ced23ffb29659157687d2da43
milton <miltondanielhch617@gmail.com>
2026-07-02 10:25:57 -0400

    Registra middleware TrackPageViews y actualiza documentación/sitemap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\TrackPageViews::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
